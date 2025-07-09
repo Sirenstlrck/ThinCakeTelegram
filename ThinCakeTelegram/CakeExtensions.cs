1556cab27c65d21be25a0ae79e032ddf0527ef1f
Sirenstlrck <meysaf204@gmail.com>
2025-07-10 00:05:49 +0700

    Initial commit

---
+++ ThinCakeTelegram/CakeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Telegram.Bot;
+
+namespace ThinCakeTelegram;
+
+public static class CakeExtensions
+{
+  [CakeMethodAlias]
+  public static void SendTelegramMessage(this ICakeContext context, string botToken, long chatId, string message)
+  {
+    var bot = new TelegramBotClient(botToken);
+    bot.SendMessage(chatId, message).Wait();
+  }
+}
